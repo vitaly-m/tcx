bfe8fa2d228b47d67c35e8b5ca51534ac1038ec9
Vitaly Merenkov <vmrnkv@gmail.com>
2021-01-07 23:12:34 +0300

    device can be parsed, unwrap removed to avoid panic

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,4 @@
-use std::io::{BufRead};
+use std::io::BufRead;
 use std::str::FromStr;
 
 use quick_xml::events::Event;
@@ -82,6 +82,77 @@ impl Version {
     }
 }
 
+/// Identifies the originating GPS device that tracked a run or
+/// used to identify the type of device capable of handling
+/// the data for loading.
+#[derive(Debug, PartialEq)]
+pub struct Device {
+    pub name: Option<String>,
+    pub unit_id: Option<u32>,
+    pub product_id: Option<u16>,
+    pub version: Option<Version>,
+}
+
+impl Device {
+    pub fn new() -> Self {
+        Self {
+            name: None,
+            unit_id: None,
+            product_id: None,
+            version: None,
+        }
+    }
+}
+
+//TODO support abstract source
+fn read_device<B: BufRead>(reader: &mut Reader<B>) -> Result<Device, String> {
+    let mut buf = Vec::new();
+    let mut d = Device::new();
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) => match e.name() {
+                b"Name" => {
+                    if let Ok(Event::Text(ref t)) = reader.read_event(&mut buf) {
+                        d.name = t.unescape_and_decode(reader).ok();
+                    }
+                }
+                b"UnitId" => {
+                    if let Ok(Event::Text(ref t)) = reader.read_event(&mut buf) {
+                        if let Ok(s) = t.unescape_and_decode(reader) {
+                            d.unit_id = u32::from_str(&s).ok();
+                        }
+                    }
+                }
+                b"ProductID" => {
+                    if let Ok(Event::Text(ref t)) = reader.read_event(&mut buf) {
+                        if let Ok(s) = t.unescape_and_decode(reader) {
+                            d.product_id = u16::from_str(&s).ok();
+                        }
+                    }
+                }
+                b"Version" => {
+                    d.version = Some(read_version(reader)?);
+                }
+                _ => (),
+            },
+            Ok(Event::End(ref e)) => match e.name() {
+                b"Creator" => break,
+                _ => (),
+            },
+            Err(e) => {
+                return Err(format!(
+                    "Error parsing Device at position {}: {:?}",
+                    reader.buffer_position(),
+                    e
+                ))
+            }
+            _ => (),
+        }
+    }
+    Ok(d)
+}
+
+//TODO support abstract source
 fn read_application<B: BufRead>(reader: &mut Reader<B>) -> Result<Application, String> {
     let mut buf = Vec::new();
     let mut a = Application::new();
@@ -182,26 +253,30 @@ fn read_version<B: BufRead>(reader: &mut Reader<B>) -> Result<Version, String> {
             Ok(Event::Start(ref e)) => match e.name() {
                 b"VersionMajor" => {
                     if let Ok(Event::Text(ref t)) = reader.read_event(&mut buf) {
-                        version.version_major =
-                            u16::from_str(&t.unescape_and_decode(&reader).unwrap()).ok();
+                        if let Ok(s) = t.unescape_and_decode(reader) {
+                            version.version_major = u16::from_str(&s).ok();
+                        }
                     }
                 }
                 b"VersionMinor" => {
                     if let Ok(Event::Text(ref t)) = reader.read_event(&mut buf) {
-                        version.version_minor =
-                            u16::from_str(&t.unescape_and_decode(&reader).unwrap()).ok();
+                        if let Ok(s) = t.unescape_and_decode(reader) {
+                            version.version_minor = u16::from_str(&s).ok();
+                        }
                     }
                 }
                 b"BuildMajor" => {
                     if let Ok(Event::Text(ref t)) = reader.read_event(&mut buf) {
-                        version.build_major =
-                            u16::from_str(&t.unescape_and_decode(&reader).unwrap()).ok();
+                        if let Ok(s) = t.unescape_and_decode(reader) {
+                            version.build_major = u16::from_str(&s).ok();
+                        }
                     }
                 }
                 b"BuildMinor" => {
                     if let Ok(Event::Text(ref t)) = reader.read_event(&mut buf) {
-                        version.build_minor =
-                            u16::from_str(&t.unescape_and_decode(&reader).unwrap()).ok();
+                        if let Ok(s) = t.unescape_and_decode(reader) {
+                            version.build_minor = u16::from_str(&s).ok();
+                        }
                     }
                 }
                 _ => (),
@@ -212,7 +287,7 @@ fn read_version<B: BufRead>(reader: &mut Reader<B>) -> Result<Version, String> {
             },
             Err(e) => {
                 return Err(format!(
-                    "Error parsing Application at position {}: {:?}",
+                    "Error parsing Version at position {}: {:?}",
                     reader.buffer_position(),
                     e
                 ))
@@ -228,8 +303,6 @@ fn read_version<B: BufRead>(reader: &mut Reader<B>) -> Result<Version, String> {
 mod tests {
     use super::*;
 
-// use test::Bencher;
-
     #[test]
     fn read_application_test() {
         let tcx_bytes: &[u8] = include_bytes!("../test_resources/+__2020-12-28_16-36-16.TCX.xml");
@@ -241,11 +314,11 @@ mod tests {
                 lang_id: Some("EN".to_string()),
                 part_number: Some("XXX-XXXXX-XX".to_string()),
                 build: Some(Build {
-                    version: Some(Version{
+                    version: Some(Version {
                         version_major: Some(0),
                         version_minor: Some(0),
                         build_major: None,
-                        build_minor: None
+                        build_minor: None,
                     }),
                     build_type: None,
                     time: None,
@@ -256,21 +329,24 @@ mod tests {
         )
     }
 
-    // #[bench]
-    // fn bench_xml_rs(b: &mut Bencher) {
-    //     let tcx_bytes: &[u8] = include_bytes!("../test_resources/+__2020-12-28_16-36-16.TCX.xml");
-    //     b.iter(|| {
-    //         let mut reader = EventReader::new(tcx_bytes);
-    //         read_version(&mut reader)
-    //     });
-    // }
-    //
-    // #[bench]
-    // fn bench_quick_xml(b: &mut Bencher) {
-    //     let tcx_bytes: &[u8] = include_bytes!("../test_resources/+__2020-12-28_16-36-16.TCX.xml");
-    //     b.iter(|| {
-    //         let mut reader = Reader::from_reader(tcx_bytes);
-    //         q_read_version(&mut reader)
-    //     });
-    // }
+    #[test]
+    fn read_device_test() {
+        let tcx_bytes: &[u8] = include_bytes!("../test_resources/+__2020-12-28_16-36-16.TCX.xml");
+        let mut reader = Reader::from_reader(tcx_bytes);
+        let device = read_device(&mut reader);
+        assert_eq!(
+            Ok(Device {
+                name: Some(String::from("Polar Vantage V")),
+                unit_id: Some(0),
+                product_id: Some(203),
+                version: Some(Version {
+                    version_major: Some(5),
+                    version_minor: Some(1),
+                    build_major: Some(0),
+                    build_minor: Some(0),
+                }),
+            }),
+            device
+        )
+    }
 }

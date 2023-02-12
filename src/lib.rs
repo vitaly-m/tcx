635ca267a47038304d7d2bc61c8b400ef65d2bc3
Vitaly Merenkov <vmrnkv@gmail.com>
2023-02-12 12:23:20 +0300

    fix doc test

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ mod types;
 /// Read the content of TCX xml data into TrainingCenterDatabase structure
 /// ```
 /// let tcx_bytes: &[u8] = include_bytes!("../test_resources/test.tcx.xml");
-/// assert_eq!(true, tcx::read(tcx_bytes).is_ok());
+/// assert_eq!(true, quick_tcx::read(tcx_bytes).is_ok());
 /// ```
 pub fn read<B: BufRead>(buf_reader: B) -> Result<TrainingCenterDatabase, ReadError> {
     let mut reader = Reader::from_reader(buf_reader);
